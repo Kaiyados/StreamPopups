db0e7d85d0d3e22964da7f51d26210396bb92db5
Kaiyados <kaiyados@gmail.com>
2018-08-25 21:57:57 -0500

    Revert "Big Update"
    This reverts commit cdf849cd0515ada6054449c8f126883dae1edc69.

--- scripts/streampopups.js
+++ scripts/streampopups.js
@@ -1,173 +1,129 @@
 //This function is directly responsible for showing the popup message object.
-function showElement(vObject, animationStyle, animationLength, displayLength)  
+function showElement(vObject, animation, animationTime, timeShown)  
 {
-    $(vObject).show(animationStyle, animationLength * 1000).delay(displayLength * 1000).hide(animationStyle, animationLength * 1000);
+    $(vObject).show(animation, animationTime * 1000).delay(timeShown * 1000).hide(animation, animationTime * 1000);
 }
 
-//This function displays a single popup object one time.
-function showPopup(popupElement, animationStyle, animationTime, displayLength) 
-{    
-    showElement($("#" + popupElement), animationStyle, animationTime, displayLength);  
-}
-
-//This function loops displaying single popup object.
-function loopPopup(popupElement, animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute) 
+//This function loops displaying single popup object, except when minutesToShow is "once." "once" tells this function
+//to display only one time.
+function loopPopup(vObject, animation, animationTime, timeShown, minutesToShow) 
 {
-	var show = false;
-	var now = new Date();
+    var now = new Date();
     var nowMinute = now.getMinutes();
-	var nowSecond = now.getSeconds();
-	var timeOutSeconds = 60;
-	
-	if(showAtTopOfMinute == true)
-	{
-		timeOutSeconds = 1;
-		
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all" && nowSecond == 0)
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0 && nowSecond == 0)
-		{
-			show = true;
-		}
-	}
-	else
-	{
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all")
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0)
-		{
-			show = true;
-		}
-	}
-	
-	if(show == true)
-	{
-		showElement($("#" + popupElement), animationStyle, animationLength, displayLength);
-	}
+    var showOnce = 0;
     
-    setTimeout(function() {loopPopup(popupElement,animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute); }, (timeOutSeconds * 1000));    
+    if( typeof minutesToShow == "string")
+    {
+        if(minutesToShow.toLowerCase() == "all" || minutesToShow.toLowerCase() == "once")
+        {
+            showElement(vObject, animation, animationTime, timeShown);
+        }
+        
+        if(minutesToShow.toLowerCase() == "once")
+        {
+            showOnce = 1;
+        }
+    }
+    else
+    {
+        if(minutesToShow.indexOf(nowMinute) >= 0)
+        {
+            showElement(vObject, animation, animationTime, timeShown);
+        }
+    }    
+    
+    if(showOnce = 0)
+    {
+        setTimeout(function() {loopPopup(vObject,animation, animationTime, timeShown, minutesToShow); }, 60000);
+    }
 }
 
-//This function displays a popup object with a random message one time.
-function showRandomMessage(popupElement, animationStyle, animationLength, displayLength, randomMessages) 
+//This function loops displaying a popup object with random messages, except when minutesToShow is "once." "once" tells
+//this function to display only one time.
+function loopRandomMessage(vObject, animation, animationTime, timeShown, minutesToShow, messages) 
 {
-	var foundObj = $("#" + popupElement);
-    foundObj.find($("#messageText")).html(randomMessages[Math.floor(Math.random() * randomMessages.length)]);
-    showElement(foundObj, animationStyle, animationLength, displayLength);    
-}
-
-//This function loops displaying a popup object with random messages.
-function loopRandomMessage(popupElement, animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute, randomMessages) 
-{
-    var show = false;
-	var now = new Date();
+    var now = new Date();
     var nowMinute = now.getMinutes();
-	var nowSecond = now.getSeconds();
-	var timeOutSeconds = 60;
+    var showOnce = 0;
     
-    if(showAtTopOfMinute == true)
-	{
-		timeOutSeconds = 1;
-		
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all" && nowSecond == 0)
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0 && nowSecond == 0)
-		{
-			show = true;
-		}
-	}
-	else
-	{
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all")
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0)
-		{
-			show = true;
-		}
-	}
-	
-	if(show == true)
-	{
-		var foundObj = $("#" + popupElement);
-		foundObj.find($("#messageText")).html(randomMessages[Math.floor(Math.random() * randomMessages.length)]);
-		showElement(foundObj, animationStyle, animationLength, displayLength);
-	}
-	
-	setTimeout(function() {loopRandomMessage(popupElement,animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute, randomMessages); }, (timeOutSeconds * 1000));
+    if( typeof minutesToShow == "string")
+    {
+        if(minutesToShow.toLowerCase() == "all" || minutesToShow.toLowerCase() == "once")
+        {
+            vObject.find($("#messageText")).html(messages[Math.floor(Math.random() * messages.length)]);
+            showElement(vObject, animation, animationTime, timeShown);
+        }
+        
+        if(minutesToShow.toLowerCase() == "once")
+        {
+            showOnce = 1;
+        }
+    }
+    else
+    {
+        if(minutesToShow.indexOf(nowMinute) >= 0)
+        {
+            vObject.find($("#messageText")).html(messages[Math.floor(Math.random() * messages.length)]);
+            showElement(vObject, animation, animationTime, timeShown);
+        }
+    }
+    
+    if(showOnce = 0)
+    {
+        setTimeout(function() {loopRandomMessage(vObject,animation, animationTime, timeShown, minutesToShow, messages); }, 60000);
+    }
 }
 
-//This function displays an array of objects, but only showing each of them one time.
-function showMultiple(popupElements, animationStyle, animationLength, displayLength, minutesToShow) 
-{   
-	popupElements.forEach(function (item, index)
-	{
-		if(index == 0)
-		{
-			showElement($("#" + item), animationStyle, animationLength, displayLength);
-		}
-		else
-		{
-			setTimeout(function() { showElement($("#" + item),animationStyle,animationLength,displayLength); }, ((((animationLength * 2) + displayLength) * 1000) + 300) * index) ;
-		}
-	});
-}
-
-//This function loops displaying an array of popup objects.
-function loopMultiple(popupElements, animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute) 
+//This function loops multiple popup objects (expecting vObject to be an array of objects). This displays the array of popup objects
+// in a loop, except when minutesToShow is "once." "once" tells this function to show the objects one time.
+function loopMultiple(vObject, animation, animationTime, timeShown, minutesToShow) 
 {
-   	var show = false;
-	var now = new Date();
+    var now = new Date();
     var nowMinute = now.getMinutes();
-	var nowSecond = now.getSeconds();
-	var timeOutSeconds = 60;
-	
-    if(showAtTopOfMinute == true)
-	{
-		timeOutSeconds = 1;
-		
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all" && nowSecond == 0)
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0 && nowSecond == 0)
-		{
-			show = true;
-		}
-	}
-	else
-	{
-		if( typeof minutesToShow == "string" && minutesToShow.toLowerCase() == "all")
-		{
-			show = true;
-		}
-		else if(minutesToShow.indexOf(nowMinute) >= 0)
-		{
-			show = true;
-		}
-	}
-	
-	if(show == true)
-	{
-		popupElements.forEach(function (item, index)
-		{
-			if(index == 0)
-			{
-				showElement($("#" + item), animationStyle, animationLength, displayLength);
-			}
-			else
-			{
-				setTimeout(function() { showElement($("#" + item),animationStyle,animationLength,displayLength); }, ((((animationLength * 2) + displayLength) * 1000) + 300) * index) ;
-			}
-		});
-	}
+    var showOnce = 0;
     
-    setTimeout(function() {loopMultiple(popupElements, animationStyle, animationLength, displayLength, minutesToShow, showAtTopOfMinute); }, (timeOutSeconds * 1000));
+    if( typeof minutesToShow == "string")
+    {
+        if(minutesToShow.toLowerCase() == "all" || minutesToShow.toLowerCase() == "once")
+        {
+            vObject.forEach(function (item, index)
+            {
+                if(index == 0)
+                {
+                    showElement(item, animation, animationTime, timeShown);
+                }
+                else
+                {
+                    setTimeout(function() { showElement(item,animation,animationTime,timeShown); }, ((((animationTime * 2) + timeShown) * 1000) + 300) * index) ;
+                }
+            });
+        }
+        
+        if(minutesToShow.toLowerCase() == "once")
+        {
+            showOnce = 1;
+        }
+    }
+    else
+    {
+        if(minutesToShow.indexOf(nowMinute) >= 0)
+        {
+            vObject.forEach(function (item, index)
+            {
+                if(index == 0)
+                {
+                    showElement(item, animation, animationTime, timeShown);
+                }
+                else
+                {
+                    setTimeout(function() { showElement(item,animation,animationTime,timeShown); }, ((((animationTime * 2) + timeShown) * 1000) + 300) * index) ;
+                }
+            });
+        }
+    }
+    
+    if(showOnce = 0)
+    {
+        setTimeout(function() { loopMultiple(vObject, animation, animationTime, timeShown, minutesToShow); }, 60000);
+    }
 }
